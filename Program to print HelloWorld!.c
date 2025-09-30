97b9102f367774415cd2b3e35a97ab6cf2ead19e
tian-lee17 <130292224+tian-lee17@users.noreply.github.com>
2025-09-30 22:04:18 +0530

    Create Program to print HelloWorld!.c
    A program to print HelloWorld using the printf statement.

---
+++ Program to print HelloWorld!.c
@@ -0,0 +1,9 @@
+/*The printf() function is an output function defined in the standard input/output header file (stdio.h). 
+Its primary purpose is to print (display) formatted output to the standard output stream, which is typically the console or terminal screen.*/
+//Syntax: printf("Format string",arg1,arg2,...);
+#include<stdio.h>
+int main()
+{
+  printf("HelloWorld!");
+  return 0;//ensures the successful execution of the code.
+}
